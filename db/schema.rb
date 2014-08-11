f36877758c1923536724c2b67951c53f0cdbbf0c
ShahanaMay <shahana@codelation.com>
2014-08-10 21:55:24 -0500

    generate seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811022843) do
+ActiveRecord::Schema.define(version: 20140811024400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140811022843) do
     t.string   "illustrator"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "series"
   end
 
   create_table "books_formats", id: false, force: true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+books = Book.create([
+  { title: "Kushiel's Dart",
+    author_first_name: "Jacqueline",
+    author_last_name: "Carey",
+    series: "Kushieline Legacy",
+    isbn: "1234567890" },
+  { title: "Fool Moon",
+    author_first_name: "Jim",
+    author_last_name: "Butcher",
+    series: "Dresden Files",
+    isbn: "2345678901" }
+])
+
+genres = Genre.create([
+  { name: "Science Fiction" },
+  { name: "Alternate History" },
+  { name: "Fantasy" },
+  { name: "Mystery" }
+])
+
+formats = Format.create([
+  { name: "Mass Market" },
+  { name: "Trade Paperback" },
+  { name: "Hardcover" },
+  { name: "E-book" }
+])
